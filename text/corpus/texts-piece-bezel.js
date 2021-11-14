13b4b5d10de29faf8628505828a07ade821d30b8
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2021-11-14 12:16:13 +0100

    docs update

---
+++ text/corpus/texts-piece-bezel.js
@@ -0,0 +1,1 @@
+corpusData["texts"]["piece"]["bezel"] = "-1\n\n-1\n\n1\n\n2\n\n3\n\n4\n\n5\n\n6\n\n7\n\n8\n\n10\n\n11\n\n12\n\n13\n\n14\n\n15\n\n16\n\n17\n\n18\n\n19\n\n20\n\n21\n\n22\n\n23\n\n24\n\n25\n\n27\n\n-1\n\n-1\n\n"
---
+++ text/corpus/texts-piece-number.js
@@ -0,0 +1,1 @@
+corpusData["texts"]["piece"]["number"] = "1\n\n3\n\n4\n\n5\n\n6\n\n7\n\n8\n\n9\n\n10\n\n11\n\n13\n\n14\n\n15\n\n16\n\n17\n\n18\n\n19\n\n20\n\n21\n\n22\n\n23\n\n24\n\n25\n\n26\n\n27\n\n28\n\n30\n\n34\n\n35\n\n"
